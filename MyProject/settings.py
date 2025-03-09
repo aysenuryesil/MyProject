ef02b07ce8f8ebe2ed3c7a3e1ea318f3c5b0922c
Ayşenur Yeşil <101892357+aysenuryesil@users.noreply.github.com>
2025-03-09 16:01:02 +0300

    render

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.contrib import messages
 from datetime import timedelta
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +113,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 
-    }
+    },
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
